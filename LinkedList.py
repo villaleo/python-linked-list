f049c2ccba32df2d4c02a3bf9001c74a5e83d570
Leonardo C Villalobos <75503696+villaleo@users.noreply.github.com>
2021-04-17 13:40:02 -0700

    Added reverse( ) and proper documentation

--- LinkedList.py
+++ LinkedList.py
@@ -1,88 +1,106 @@
 # -----------------------------------------
 # Author: Leonardo Villalobos
-# Date: 4/12/2021
-# Description: Construct a Linked List data
+# Date: 4/17/2021
+# Description: Construct a linked list data
 # structure class with various methods.
 # -----------------------------------------
 import Node as nd
 
 
 class LinkedList:
+    """
+    Construct a linked list data structure.
+
+    + Add items using `addFirst(item: int)` or `addLast(item: int)`.
+
+    + Delete items using `deleteFirst()` or `deleteLast()`.
+
+    + Check if an item is in the linked list using `contains(item: int)`.
+
+    + Grab the index of the first occurance of an item using `indexOf(item: int)`.
+
+    + Grab the size of the linked list using `size()`.
+
+    + Make a list representation of the linked list using `toList()`.
+
+    + Reverse the linked list in place using `reverse()`.
+    """
     first: nd.Node
     last: nd.Node
     count: int
 
     def __init__(self):
+        """
+        Construct an empty linked list.
+        """
         self.first = None
         self.last = None
         self.count = 0
 
-    def isEmpty(self) -> bool:
+    def __isEmpty(self) -> bool:
         """
-        Returns True if the Linked List is empty.
+        Returns `True` if the linked list is empty.
         """
         return self.first == None
 
-    def getPrevious(self, node: nd.Node) -> nd.Node:
+    def __getPrevious(self, node: nd.Node) -> nd.Node:
         """
         Returns the previous node of a node object.
 
-        *node: nd.node -- A node object.
+        + `node: nd.node` -- A node object.
         """
-        if self.isEmpty():
-            raise TypeError(
-                "There is no previous item, for the list is empty.")
+        if self.__isEmpty():
+            raise TypeError("Cannot get previous item from empty linked list.")
         if self.first == self.last:
             self.first = self.last = None
+
         current = self.first
+
         while current != None:
             if current.nextNode == node:
                 return current
             current = current.nextNode
+
         return None
 
     def addFirst(self, item: int) -> None:
         """
-        Add item to the beggining of the Linked List.
+        Add `item` to the beginning of the linked list.
 
-        *item: int -- the item being added to the front of the Linked List.
+        + `item: int` -- the item which will be added.
         """
         node = nd.Node(item)
-        # create a variable, node, which holds a new node
-        # object with the item assigned to the value.
-        if self.isEmpty():
+
+        if self.__isEmpty():
             self.first = self.last = node
-            # if the front of the LinkedList is empty, set the
-            # first and last (front and back) of the LinkedList
-            # to point to 'node'.
         else:
             node.nextNode = self.first
             self.first = node
-            # otherwise, the current node's 'nextNode' variable
-            # points to the this front (first) and this front
-            # (first) points to the 'node'.
+
         self.count += 1
 
     def addLast(self, item: int) -> None:
         """
-        Add item to the end of the Linked List.
+        Add `item` to the end of the linked list.
 
-        *item: int -- the item to add to the back of the Linked List.
+        + `item: int` -- the item which will be added.
         """
         node = nd.Node(item)
-        if (self.isEmpty()):
+
+        if (self.__isEmpty()):
             self.first = self.last = node
         else:
             self.last.nextNode = node
             self.last = node
+
         self.count += 1
 
     def deleteFirst(self) -> None:
         """
-        Delete the first item in the Linked List.
+        Delete the first item in the linked list.
         """
-        if self.isEmpty():
-            raise TypeError("Cannot remove item from empty list")
+        if self.__isEmpty():
+            raise TypeError("Cannot remove item from empty linked list")
         if self.first == self.last:
             self.first = self.last == None
             return
@@ -90,65 +108,87 @@ class LinkedList:
             second = self.first.nextNode
             self.first.nextNode = None
             self.first = second
+
         self.count -= 1
 
     def deleteLast(self) -> None:
         """
-        Delete the last item in the Linked List.
+        Delete the last item in the linked list.
         """
-        if self.isEmpty():
-            raise TypeError("Cannot remove item from empty list")
+        if self.__isEmpty():
+            raise TypeError("Cannot remove item from empty linked list")
         if self.first == self.last:
             self.first = self.last = None
         else:
-            previous = self.getPrevious(self.last)
+            previous = self.__getPrevious(self.last)
             self.last = previous
             self.last.nextNode = None
+
         self.count -= 1
 
     def contains(self, item: int) -> bool:
         """
-        Returns True if the item is found within
-        the LinkedList. False otherwise.
+        Returns `True` if `item: int` is found within
+        the linked list. `False` otherwise.
 
-        *item: int -- the item being search for within the Linked List.
+        + `item: int` -- the item being search for within the linked list.
         """
         return self.indexOf(item) != -1
 
     def indexOf(self, item: int) -> int:
         """
-        Returns the index of the first occurence of the item.
+        Returns the index of the first occurence of `item`.
 
-        *item: int -- the item within the Linked List.
+        + `item: int` -- the item in question.
         """
         index = 0
         current = self.first
-        # current gets assigned to the first node
+
         while current != None:
             if current.value == item:
-                # if the current node's value is equal
-                # to the item we are looking for,
-                # return True
                 return index
-            # update current node to the next node
             current = current.nextNode
             index += 1
         return -1
 
     def size(self) -> int:
         """
-        Returns the size of the Linked List.
+        Returns the size of the linked list.
         """
         return self.count
 
     def toList(self) -> list:
         """
-        Returns a list representation of the Linked List
+        Returns a list representation of the linked list
         """
-        temp = []
+        temp = list()
         index = 0
         current = self.first
+
         while current != None:
             temp.append(current.value)
             current = current.nextNode
         return temp
+
+    def reverse(self) -> None:
+        """
+        Reverses the linked list.
+        """
+        if self.__isEmpty():
+            raise TypeError("Cannot reverse empty linked list.")
+        if self.count == 1:
+            return
+
+        track = self.last
+        prev = self.__getPrevious(track)
+
+        while prev != None:
+            self.addLast(prev)
+            prev = self.__getPrevious(prev)
+
+        prev = self.first
+
+        while self.first != track:
+            self.first = prev.nextNode
+            prev.nextNode = None
+            prev = self.first
