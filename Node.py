c92d100d6f7057d33707befe707c57f31504d0cd
Leonardo C Villalobos <75503696+villaleo@users.noreply.github.com>
2021-04-17 13:40:58 -0700

    Add proper documentation

--- Node.py
+++ Node.py
@@ -7,11 +7,21 @@
 # -----------------------------------------
 
 class Node:
+    """
+    Construct a node object. Each node holds a `value: int` and
+    a reference to the next node, `nextNode: Node`.
+    """
     value = 0
     nextNode = None
 
-    def __init__(self, value):
+    def __init__(self, value: int):
+        """
+        Construct a node object with `value` assigned for the value.
+        """
         self.value = value
 
     def __repr__(self):
+        """
+        String representation of the node object.
+        """
         return str(self.value)
